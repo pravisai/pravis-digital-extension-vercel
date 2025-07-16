9563c0e69ad0808dbacdf8edad3673979c1b79b0
p s <dr.pranavshimpi@gmail.com>
2025-07-16 16:15:11 +0000

    404 This page could not be found.

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,13 +3,13 @@ import { getApp, getApps, initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+    apiKey: "AIzaSyCYdzbPrsQ7vW6UrdpgSeN5KW85kzGliPA",
+    authDomain: "pravis-your-digital-extension.firebaseapp.com",
+    projectId: "pravis-your-digital-extension",
+    storageBucket: "pravis-your-digital-extension.firebasestorage.app",
+    messagingSenderId: "827924117533",
+    appId: "1:827924117533:web:51d4b9d9ba16721bbbeef4"
+  };
 
 function isConfigValid(config: typeof firebaseConfig): boolean {
     return Object.values(config).every(value => !!value);
