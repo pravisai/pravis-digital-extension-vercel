0db94205566231229963e5ff64ed2859a1b81813
p s <dr.pranavshimpi@gmail.com>
2025-07-20 03:35:25 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,8 @@ export default function RootLayout({
       <head>
         <meta http-equiv="Content-Security-Policy" content="script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://*.firebaseio.com https://www.googleapis.com;" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-        <meta name="mobile-web-app-capable" content="yes"></meta>
+        <meta name="mobile-web-app-capable" content="yes">
+</meta>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#BF5FFF" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
