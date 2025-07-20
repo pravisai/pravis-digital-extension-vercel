65e887833192558c4a99d1d2c2162550a0dd37e9
p s <dr.pranavshimpi@gmail.com>
2025-07-20 03:52:00 +0000

    Fix template literal CSP formatting for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -22,35 +22,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' 
-                https://apis.google.com 
-                https://accounts.google.com 
-                https://www.gstatic.com 
-                https://www.googleapis.com 
-                https://*.firebaseio.com
-                blob:;
-              script-src-elem 'self' 'unsafe-inline' 
-                https://apis.google.com 
-                https://accounts.google.com 
-                https://www.gstatic.com 
-                https://www.googleapis.com
-                blob:;
-              style-src 'self' 'unsafe-inline' 
-                https://accounts.google.com 
-                https://fonts.googleapis.com;
-              font-src 'self' 
-                https://fonts.gstatic.com;
-              connect-src 'self' 
-                https://identitytoolkit.googleapis.com 
-                https://securetoken.googleapis.com 
-                https://accounts.google.com
-                https://*.firebaseio.com;
-              frame-src 'self' 
-                https://accounts.google.com;
-              img-src 'self' data: https:;
-            `.replace(/\s+/g, ' ').trim()
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com https://*.firebaseio.com blob:; script-src-elem 'self' 'unsafe-inline' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com blob:; style-src 'self' 'unsafe-inline' https://accounts.google.com https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://accounts.google.com https://*.firebaseio.com; frame-src 'self' https://accounts.google.com; img-src 'self' data: https:;"
           }
         ]
       }
