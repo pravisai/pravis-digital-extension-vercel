3381b1bbb679b3950d66af2daafc4123cfe7ccab
p s <dr.pranavshimpi@gmail.com>
2025-07-20 05:44:04 +0000

    fix: final verified Firebase Google auth CSP & config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
@@ -25,6 +21,7 @@ const nextConfig = {
             value:
               "default-src 'self'; " +
               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com https://*.firebaseio.com blob:; " +
+              "script-src-elem 'self' 'unsafe-inline' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com https://*.firebaseio.com blob:; " +
               "style-src 'self' 'unsafe-inline' https://accounts.google.com https://fonts.googleapis.com; " +
               "font-src 'self' https://fonts.gstatic.com; " +
               "connect-src 'self' https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://accounts.google.com https://*.firebaseio.com; " +
