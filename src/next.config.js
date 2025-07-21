542d8316e6d246f59ea3cc53830a8d5875867245
p s <dr.pranavshimpi@gmail.com>
2025-07-21 09:30:16 +0000

    accounts.google.com refused to connect

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ... your other settings like typescript, eslint, webpack ...
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@opentelemetry/exporter-jaeger': false,
+      '@opentelemetry/exporter-prometheus': false,
+      '@opentelemetry/exporter-zipkin': false,
+    };
+    return config;
+  },
   async headers() {
     return [
       {
@@ -9,14 +19,18 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'self' https://*.firebaseio.com; " +
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com https://*.firebaseio.com blob:; " +
-              "script-src-elem 'self' 'unsafe-inline' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com blob:; " +
+              "default-src 'self'; " +
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com https://*.firebaseapp.com https://*.firebaseio.com blob:; " +
+              "script-src-elem 'self' 'unsafe-inline' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com https://*.firebaseapp.com https://*.firebaseio.com blob:; " +
               "style-src 'self' 'unsafe-inline' https://accounts.google.com https://fonts.googleapis.com; " +
               "font-src 'self' https://fonts.gstatic.com; " +
-              "connect-src 'self' wss://*.firebaseio.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://accounts.google.com https://*.firebaseio.com; " +
+              "connect-src 'self' https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://accounts.google.com https://*.firebaseapp.com https://*.firebaseio.com; " +
               "frame-src 'self' https://accounts.google.com https://pravis-your-digital-extension.firebaseapp.com; " +
               "img-src 'self' data: https:;"
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate'
           }
         ]
       }
