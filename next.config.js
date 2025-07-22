668fdccc40dbdde65ac291126c4f93ecf8cc1d43
p s <dr.pranavshimpi@gmail.com>
2025-07-22 07:24:41 +0000

    The `npm install` command failed in my project. Analyze the following er

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  images: {
-    unoptimized: true
-  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
@@ -23,18 +20,17 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value:
               "default-src 'self'; " +
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com https://*.firebaseapp.com https://*.firebaseio.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com blob:; " +
-              "script-src-elem 'self' 'unsafe-inline' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com https://*.firebaseapp.com https://*.firebaseio.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com blob:; " +
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com https://*.firebaseapp.com https://*.firebaseio.com; " +
+              "script-src-elem 'self' 'unsafe-inline' https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com https://*.firebaseapp.com https://*.firebaseio.com; " +
               "style-src 'self' 'unsafe-inline' https://accounts.google.com https://fonts.googleapis.com; " +
               "font-src 'self' https://fonts.gstatic.com; " +
-              "connect-src 'self' https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://accounts.google.com https://*.firebaseapp.com https://*.firebaseio.com; " +
-              "frame-src 'self' https://accounts.google.com https://*.firebaseapp.com https://pravis-your-digital-extension.firebaseapp.com; " +
-              "img-src 'self' data: https:; " +
-              "object-src 'none'; base-uri 'self';"
+              "connect-src 'self' https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://accounts.google.com https://*.firebaseapp.com https://*.firebaseio.com https://www.googleapis.com https://generativelanguage.googleapis.com; " +
+              "frame-src 'self' https://accounts.google.com https://pravis-your-digital-extension.firebaseapp.com; " +
+              "img-src 'self' data: https:;"
           },
           {
             key: 'Cache-Control',
-            value: 'public, max-age=3600'
+            value: 'no-cache, no-store, must-revalidate'
           }
         ]
       }
