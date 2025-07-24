fb1ce63a4aa9e05d9686ebade6025688b2207ff3
p s <dr.pranavshimpi@gmail.com>
2025-07-24 04:23:08 +0000

    u have all the api keys and everything in the code make this pravis chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ function RootLayoutClient({
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#BF5FFF" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet" />
