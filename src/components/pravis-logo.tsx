748a4432da65ffbdb5386eee46a68cc490a6ba06
p s <dr.pranavshimpi@gmail.com>
2025-07-26 07:21:56 +0000

    i want the design to be 3d and moving in space like the one you have cre

--- src/components/pravis-logo.tsx
+++ src/components/pravis-logo.tsx
@@ -8,33 +8,21 @@ interface PravisLogoProps {
     size?: number;
 }
 
-export function PravisLogo({ className, size = 50 }: PravisLogoProps) {
-    const faceStyle = {
+export function PravisLogo({ className, size = 60 }: PravisLogoProps) {
+    const wrapperStyle = {
         width: `${size}px`,
         height: `${size}px`,
     };
-
-    const cubeStyle = {
-        width: `${size}px`,
-        height: `${size}px`,
-    };
-
-    const frontTransform = { transform: `rotateY(0deg) translateZ(${size / 2}px)` };
-    const rightTransform = { transform: `rotateY(90deg) translateZ(${size / 2}px)` };
-    const backTransform = { transform: `rotateY(180deg) translateZ(${size / 2}px)` };
-    const leftTransform = { transform: `rotateY(-90deg) translateZ(${size / 2}px)` };
-    const topTransform = { transform: `rotateX(90deg) translateZ(${size / 2}px)` };
-    const bottomTransform = { transform: `rotateX(-90deg) translateZ(${size / 2}px)` };
 
     return (
-        <div className={cn("logo-scene", className)} style={{ width: `${size}px`, height: `${size}px` }}>
-            <div className="logo-cube" style={cubeStyle}>
-                <div className="logo-cube__face logo-cube__face--front" style={{ ...faceStyle, ...frontTransform }}></div>
-                <div className="logo-cube__face logo-cube__face--right" style={{ ...faceStyle, ...rightTransform }}></div>
-                <div className="logo-cube__face logo-cube__face--back" style={{ ...faceStyle, ...backTransform }}></div>
-                <div className="logo-cube__face logo-cube__face--left" style={{ ...faceStyle, ...leftTransform }}></div>
-                <div className="logo-cube__face logo-cube__face--top" style={{ ...faceStyle, ...topTransform }}></div>
-                <div className="logo-cube__face logo-cube__face--bottom" style={{ ...faceStyle, ...bottomTransform }}></div>
+        <div className={cn("transforming-logo-scene", className)} style={wrapperStyle}>
+            <div className="transforming-logo-pivot">
+                <div className="transforming-logo-m">
+                    <div className="transforming-logo-part part-1"></div>
+                    <div className="transforming-logo-part part-2"></div>
+                    <div className="transforming-logo-part part-3"></div>
+                    <div className="transforming-logo-part part-4"></div>
+                </div>
             </div>
         </div>
     );
